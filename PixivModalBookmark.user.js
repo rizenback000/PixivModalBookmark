9900f0b46a2b191eb5a5b3b6b225f20af5992625
rizenback000 <10810847+rizenback000@users.noreply.github.com>
2019-04-10 22:36:07 +0900

    updated 2.0.3

--- PixivModalBookmark.user.js
+++ PixivModalBookmark.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         PixvModalBookmark
 // @namespace    http://unote.hatenablog.com/
-// @version      2.0.2
+// @version      2.0.3
 // @description  Pixivのブックマークをモーダル表示させる
 // @author       rizenback000
-// @match        https://www.pixiv.net/member_illust.php?*
+// @match        https://www.pixiv.net/member_illust.php?*illust_id=*
 // @remark       https://code.jquery.com/jquery-3.3.1.slim.min.js //使わないことにした
 // @grant        none
 // ==/UserScript==
@@ -14,72 +14,113 @@
 // 1.3.0　jQuery使わないバージョン
 // 2.0.0  iframeを使ったバージョン
 // 2.0.1　仕様変更に対応
-// 2.0.2　バグ対応(確実に読み込めなかった)
+// 2.0.3　バグ対応(確実に読み込めなかった)
 (function () {
   'use strict';
+  const modal = document.createElement('div');
+  const iframe = document.createElement('iframe');
+
+  const winLoad = () => {
+    console.log('load');
+    // 画像部分の読み込みは後から読み込まれるので、
+    // まずg[mask]が読み込まれるのを確認する
+    const target = document.querySelector('body');
+    const observer = new MutationObserver(() => {
+      const g = document.querySelector('g[mask]');
+      if (g !== null) {
+        // 最初だけ追加
+        if (modal.id === '') {
+          modal.id = 'modalContainer';
+          modal.style.position = "fixed";
+          modal.style.display = "none";
+          modal.style.width = "100%";
+          modal.style.height = "100%";
+          modal.style.top = "0";
+          modal.style.left = "0";
+          modal.style.padding = 'auto auto';
+          modal.style.backgroundColor = "rgba(0,0,0,0.5)";
+          modal.style.zIndex = "2";
+          modal.style.cursor = "pointer";
+          document.body.appendChild(modal);
+
+          iframe.id = 'modalIframe';
+          iframe.style.width = '50%';
+          iframe.style.height = '50%';
+          iframe.style.margin = 'auto';
+          iframe.style.top = "0";
+          iframe.style.left = "0";
+          iframe.style.right = "0";
+          iframe.style.bottom = "0";
+          iframe.style.position = "absolute";
+
+          iframe.onload = () => {
+            const ifcont = iframe.contentDocument;
+            if (ifcont.URL == "about:blank") return 1;
+            const header = ifcont.querySelector('header');
+            const toolmenu = ifcont.querySelector('._toolmenu');
+            const footer = ifcont.querySelector('footer');
+            //const feedback = ifcont.querySelector('#toolbar-items');
+            const thumb = ifcont.querySelector('.thumbnail-container');
+            //feedback.parentNode.removeChild(feedback);
+            header.parentNode.removeChild(header);
+            toolmenu.parentNode.removeChild(toolmenu);
+            footer.parentNode.removeChild(footer);
+            thumb.parentNode.removeChild(thumb);
+            const bmDetail = ifcont.querySelector('section.bookmark-detail-unit');
+            bmDetail.style.padding = "0px 5px";
+            const exTags = ifcont.querySelector('._unit._list-unit.bookmark-list-unit');
+            exTags.style.padding = "0px 5px";
+            const inputBoxes = ifcont.querySelectorAll('div.input-box');
+            Array.from(inputBoxes, elm => elm.style.marginLeft = "0");
+            const userIcon = ifcont.querySelector('div._user-icon');
+            userIcon.style.top = "0";
+            userIcon.style.left = "0";
+            const wrapper = ifcont.querySelector('#wrapper');
+            wrapper.style.width = 'auto';
+            wrapper.style.marginTop = '0';
+            const lbody = ifcont.querySelector('.layout-body');
+            lbody.style.marginTop = '0';
+            lbody.style.width = 'auto';
+            const form = ifcont.querySelector('form');
+            form.style.paddingTop = '10';
+          };
+          modal.appendChild(iframe);
+        }
+
+        // イベントの追加
+        overrideFavEvent();
+        // 作品一覧から別作品へ飛ぶと画像部分だけの読み込みになるのでここでキャッチ
+        const target2 = document.querySelector('#root div[role] a');
+        const observer2 = new MutationObserver(overrideFavEvent);
+        observer2.observe(target2, {
+          attributes: true,
+          attributeFilter: ['href']
+        });
+        observer.disconnect();
+      }
+    });
+    observer.observe(target, {
+      childList: true,
+      subtree: true,
+    });
+  };
+  window.removeEventListener('load', winLoad);
+  window.addEventListener('load', winLoad);
+
+
+
 
   function overrideFavEvent() {
-    const iframe = document.createElement('iframe');
     const favIcon = document.querySelector('g[mask]').parentNode.parentNode;
     console.log(favIcon);
-    if (favIcon !== null && document.querySelector('#modal') === null) {
-      const modal = document.createElement('div');
-      modal.id = 'modal';
-      modal.style.position = "fixed";
-      modal.style.display = "none";
-      modal.style.width = "100%";
-      modal.style.height = "100%";
-      modal.style.top = "0";
-      modal.style.left = "0";
-      modal.style.padding = 'auto auto';
-      modal.style.backgroundColor = "rgba(0,0,0,0.5)";
-      modal.style.zIndex = "2";
-      modal.style.cursor = "pointer";
-      document.body.appendChild(modal);
-      iframe.style.width = '50%';
-      iframe.style.height = '50%';
-      iframe.style.margin = 'auto';
-      iframe.style.top = "0";
-      iframe.style.left = "0";
-      iframe.style.right = "0";
-      iframe.style.bottom = "0";
-      iframe.style.position = "absolute";
 
-      iframe.onload = () => {
-        const ifcont = iframe.contentDocument;
-        if (ifcont.URL == "about:blank") return 1;
-        const header = ifcont.querySelector('header');
-        const toolmenu = ifcont.querySelector('._toolmenu');
-        const footer = ifcont.querySelector('footer');
-        //const feedback = ifcont.querySelector('#toolbar-items');
-        const thumb = ifcont.querySelector('.thumbnail-container');
-        //feedback.parentNode.removeChild(feedback);
-        header.parentNode.removeChild(header);
-        toolmenu.parentNode.removeChild(toolmenu);
-        footer.parentNode.removeChild(footer);
-        thumb.parentNode.removeChild(thumb);
-        const bmDetail = ifcont.querySelector('section.bookmark-detail-unit');
-        bmDetail.style.padding = "0px 5px";
-        const exTags = ifcont.querySelector('._unit._list-unit.bookmark-list-unit');
-        exTags.style.padding = "0px 5px";
-        const inputBoxes = ifcont.querySelectorAll('div.input-box');
-        Array.from(inputBoxes, elm => elm.style.marginLeft = "0");
-        const userIcon = ifcont.querySelector('div._user-icon');
-        userIcon.style.top = "0";
-        userIcon.style.left = "0";
-        const wrapper = ifcont.querySelector('#wrapper');
-        wrapper.style.width = 'auto';
-        wrapper.style.marginTop = '0';
-        const lbody = ifcont.querySelector('.layout-body');
-        lbody.style.marginTop = '0';
-        lbody.style.width = 'auto';
-        const form = ifcont.querySelector('form');
-        form.style.paddingTop = '10';
-      };
-      modal.appendChild(iframe);
 
-    }
+    const getContentsUrl = ()=> {
+      const contentsId = document.querySelector('a[title=ブックマーク]').href.match(/\d+/)[0];
+      return "https://www.pixiv.net/bookmark_add.php?type=illust&illust_id=" + contentsId;
+    };
 
+    const getTimeoutId = () => {return favIcon.getAttribute('timeoutId');}
 
     const modalClick = () => {
       modal.style.display = 'none'
@@ -92,6 +133,12 @@
       e.preventDefault();
       e.stopPropagation();
       modal.style.display = 'block';
+      const preloadFlg = favIcon.getAttribute('preload');
+      console.log(preloadFlg);
+      if (preloadFlg !== 'preload'){
+        iframe.src = getContentsUrl();
+      }
+      favIcon.removeAttribute('preload');
     };
     favIcon.removeEventListener('click', favClick);
     favIcon.addEventListener('click', favClick);
@@ -99,10 +146,9 @@
     // iframeプリロード
     const favEnter = e => {
       const timeoutId = setTimeout((e) => {
-        const contentsId = document.querySelector('a[title=ブックマーク]').href.match(/\d+/)[0];
-        iframe.src = "https://www.pixiv.net/bookmark_add.php?type=illust&illust_id=" + contentsId;
-        console.log('enter');
-      }, 700);
+        favIcon.setAttribute('preload', 'preload');
+        iframe.src = getContentsUrl();
+      }, 500);
       favIcon.setAttribute('timeoutId', timeoutId);
     };
     favIcon.removeEventListener('mouseenter', favEnter);
@@ -111,43 +157,11 @@
 
     // すぐにマウスが離れたらプリロードさせない
     const favLeave = e => {
-      console.log('clearTimeout');
-      const timeoutId = favIcon.getAttribute('timeoutId');
-      clearTimeout(timeoutId);
+      console.log('leave');
+      clearTimeout(getTimeoutId());
     };
     favIcon.removeEventListener('mouseleave', favLeave);
     favIcon.addEventListener('mouseleave', favLeave);
   }
-
- 　
-  const winLoad = () => {
-    console.log('load');
-    // 画像部分の読み込みは後から読み込まれるので、
-    // まずg[mask]が読み込まれるのを確認する
-    const target = document.querySelector('body');
-    const observer = new MutationObserver(() => {
-      const g = document.querySelector('g[mask]');
-      if (g !== null) {
-        (function () {
-          overrideFavEvent();
-          // 作品一覧から別作品へ飛ぶと画像部分だけの読み込みになるのでここでキャッチ
-          const target2 = document.querySelector('#root div[role] a');
-          const observer2 = new MutationObserver(overrideFavEvent);
-          observer2.observe(target2, {
-            attributes: true,
-            attributeFilter: ['href']
-          });
-        })();
-        observer.disconnect();
-      }
-    });
-    observer.observe(target, {
-      childList: true,
-      subtree: true,
-    });
-  };
-  window.removeEventListener('load', winLoad);
-  window.addEventListener('load', winLoad);
-
 
 })();
